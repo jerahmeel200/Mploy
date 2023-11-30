79e785b32e2b01a5d05816c209bafaff5ab3d791
areaboxer <jamicojerahmeel@gmail.com>
2023-11-30 11:32:13 +0100

    another one

--- src/component/landingPage/hero/Hero.jsx
+++ src/component/landingPage/hero/Hero.jsx
@@ -7,22 +7,23 @@ const Hero = () => {
   return (
     <>
       <div className={styles.container}>
-        {/* <div className="main"> */}
-        <div className={styles.post}>
-          <div className={styles.textContainer}>
-            <h1 className={styles.title}>
-              Transform your recruitment process.
-            </h1>
-            <p className={styles.postDesc}>
-              Talent Acquisition System built for hiring managers and
-              recruiters, to source and hire candidates faster.
-            </p>
-            <div className={styles.btn}>
-              <button>Read More</button>
+        <div className="main">
+          <div className={styles.post}>
+            <div className={styles.textContainer}>
+              <h1 className={styles.title}>
+                Transform your recruitment process.
+              </h1>
+              <p className={styles.postDesc}>
+                Talent Acquisition System built for hiring managers and
+                recruiters, to source and hire candidates faster.
+              </p>
+              <div className={styles.btn}>
+                <button>Read More</button>
+              </div>
             </div>
-          </div>
-          <div className={styles.imgContainer}>
-            <img src="/heroImg.png" alt="" className={styles.image} />
+            <div className={styles.imgContainer}>
+              <img src="/heroImg.png" alt="" className={styles.image} />
+            </div>
           </div>
         </div>
       </div>
