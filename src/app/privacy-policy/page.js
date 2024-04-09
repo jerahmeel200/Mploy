1fabc401bfd10b4d6d946bbf14c948a01072f024
Josiah Arkson <158191169+josiah-lintechgroup@users.noreply.github.com>
2024-04-09 11:59:45 +0100

    Create page.js

---
+++ src/app/privacy-policy/page.js
@@ -0,0 +1,16 @@
+import Contact from "@/component/contactPage/contact/Contact";
+import Footer from "@/component/footer/Footer";
+import Navbar from "@/component/navbar/Navbar";
+import React from "react";
+
+const ContactPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
+
+export default ContactPage;
